730ee274dd13e9969a367b10f1b9fcd6057432f4
HangryDev <49356933+HangryDev@users.noreply.github.com>
2023-01-07 21:27:53 +0900

    Time: 49 ms (86.24%), Space: 13.8 MB (75.46%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,31 +1,18 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # 1 찾아서 pop하기. 
-        # 특수 - IV, IX, XL, XC, CD, CM << 두개씩 세야 함
-        # 나머지는 COUNT로 처리 = n^2 
-        
-        # 2 왼쪽부터 처리 (~2n)
-        # 2-1 왼쪽부터 하나씩 처리
-        # I,X,C가 나오면 2개 봄. 
-        ### 마지막이라 한개거나, 특수에 해당 안하면 그냥 pop해서 처리 
-        
-        sp = ["CM", "CD", "XC", "XL", "IX", "IV"]
-        spv = [900, 400, 90, 40, 9, 4]
-        nrm = ["M", "D", "C", "L", "X", "V", "I"]
-        nrmv = [1000,500,100,50,10,5,1]
-        
-        total = 0
-        
-        while s: 
-            pointer = s[0]
-            s = s[1:]
-            if len(s) != 0 and pointer + s[0] in sp :
-                total += spv[sp.index(pointer + s[0])]
-                s = s[1:]
-            else: 
-                total += nrmv[nrm.index(pointer)]
-                
-        return total
-        
-        
-        
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
