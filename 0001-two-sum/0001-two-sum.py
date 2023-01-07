652491db865285d44ded9ca7bfe733a66984222f
HangryDev <49356933+HangryDev@users.noreply.github.com>
2023-01-07 20:01:53 +0900

    Time: 65 ms (85.14%), Space: 15.2 MB (49.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0,len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j] 
-        return []
-
-
-
-#1 nums에서 숫자 하나를 고른다
-#2 target에서 #1을 뺀 값이 있는지 확인한다. 없으면 다시 한다. 
-# big o = n^2 
-
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i 
+        for i in range(len(nums)):
+            complement =  target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i,hashmap[complement]]
+                
+        
