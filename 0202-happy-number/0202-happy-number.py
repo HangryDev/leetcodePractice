d4694f3d167590dad64626f6e37d3f884c1f22c6
HangryDev <49356933+HangryDev@users.noreply.github.com>
2023-02-28 18:12:42 +0900

    Time: 29 ms (94.31%), Space: 13.7 MB (95.56%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -2,7 +2,7 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         number = str(n)
         
-        for j in range(1,100):
+        for j in range(1,30):
             newnumber = 0 
             for i in str(number):
                 newnumber += (int(i))**2
