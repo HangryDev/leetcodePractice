476bedeb44f17ee1f58a73cb411fe263ec41d5b1
HangryDev <49356933+HangryDev@users.noreply.github.com>
2023-02-28 18:12:15 +0900

    Time: 281 ms (5.22%), Space: 13.9 MB (10.77%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        number = str(n)
+        
+        for j in range(1,1000):
+            newnumber = 0 
+            for i in str(number):
+                newnumber += (int(i))**2
+            number = newnumber
+            if number == 1:
+                return True
+        
+        return False
