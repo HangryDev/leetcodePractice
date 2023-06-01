9239b3a636be3545e3f80ef85c6ff72007d19786
HangryDev <49356933+HangryDev@users.noreply.github.com>
2023-06-01 23:15:50 +0900

    Time: 83 ms (66.74%), Space: 16.4 MB (29.89%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,20 +1,14 @@
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        mapp = [0] * 26
-        for c in s1:
-            mapp[ord(c) - 97] += 1
-        i, j, count_chars = 0, 0, len(s1)
-        while j < len(s2):
-            if mapp[ord(s2[j]) - 97] > 0:   
-                count_chars -= 1
-            mapp[ord(s2[j]) - 97] -= 1
-            j += 1
-            if count_chars == 0:
+    def checkInclusion(self, s1, s2):
+        l1 = [0]*26
+        l2 = [0]*26
+        for x in s1:
+            l1[ord(x) - ord('a')] += 1
+        
+        for i in range(len(s2)):
+            l2[ord(s2[i]) - ord('a')] += 1
+            if i >= len(s1):
+                l2[ord(s2[i-len(s1)]) - ord('a')] -= 1
+            if l1 == l2:
                 return True
-            if j - i == len(s1):
-                if mapp[ord(s2[i]) - 97] >= 0:
-                    count_chars += 1
-                mapp[ord(s2[i]) - 97] += 1
-                i += 1
-                
         return False
